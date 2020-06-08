89f376d5cf68ae4a54cb9354eb8d3e1bc301de26
mandimartins <mandimartins23@gmail.com>
2020-06-08 10:20:07 -0300

    Validate user Input with regex for android

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,23 +1,66 @@
-import React from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 
 import Card from '../components/Card';
+import Input from '../components/Input';
+
+import Colors from '../constants/colors';
 
 const StartGameScreen = (props) => {
+  const [enteredValue, setEnteredValue] = useState('');
+
+  const numberInputHabdler = (inputText) => {
+    setEnteredValue(inputText.replace(/[^0-9]/gi, ''));
+  };
+
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>Start a New Game</Text>
-      <Card style={styles.inputContainer}>
-        <View style={styles.inputContainer}>
-          <Text>Select a number</Text>
-          <TextInput />
-          <View style={styles.buttonContainer}>
-            <Button title="Reset" onPress={() => {}} />
-            <Button title="Confirm" />
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <View style={styles.screen}>
+        <Text style={styles.title}>Start a New Game</Text>
+        <Card style={styles.inputContainer}>
+          <View style={styles.inputContainer}>
+            <Text>Select a number</Text>
+            <Input
+              style={styles.input}
+              blurOnSubmit
+              autoCapitalize="none"
+              autoCorrect={false}
+              keyboardType="number-pad"
+              maxLength={2}
+              onChangeText={numberInputHabdler}
+              value={enteredValue}
+            />
+            <View style={styles.buttonContainer}>
+              <View style={styles.button}>
+                <Button
+                  title="Reset"
+                  color={Colors.accent}
+                  onPress={() => {}}
+                />
+              </View>
+              <View style={styles.button}>
+                <Button
+                  title="Confirm"
+                  color={Colors.primary}
+                  onPress={() => {}}
+                />
+              </View>
+            </View>
           </View>
-        </View>
-      </Card>
-    </View>
+        </Card>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -41,6 +84,13 @@ const styles = StyleSheet.create({
     width: '100%',
     justifyContent: 'space-between',
     paddingHorizontal: 15,
+  },
+  button: {
+    width: 100,
+  },
+  input: {
+    width: 50,
+    textAlign: 'center',
   },
 });
 
